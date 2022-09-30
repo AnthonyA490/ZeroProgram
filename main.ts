c59c08f7359e81c8db648dce1bde331567d1f241
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-30 10:43:42 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+function TEST_ON () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+}
+input.onButtonPressed(Button.A, function () {
+    TEST_ON()
+})
+input.onButtonPressed(Button.B, function () {
+    TEST_OFF()
+})
+function TEST_OFF () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+}
 basic.showIcon(IconNames.Angry)
